9c279d00a2fb5f0ea39eb6a42d37f5659a4b0832
%%GITNAME%% <%%GITEMAIL%%>
2016-10-11 20:22:37 +0700

    update

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,5 @@
 Rails.application.routes.draw do
-  get 'photos/index'
-
-  get 'photos/show'
-
+  resources :photos
   root 'photos#index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
